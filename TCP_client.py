94a066434d2b570e5f33979bfcd2884988a1358b
Elslin <wn-wf@hotmail.com>
2020-03-23 19:33:45 +0800

    Added TCP Client and Server

---
+++ TCP_client.py
@@ -0,0 +1,24 @@
+import socket
+import sys
+
+# Create a TCP/IP Socket
+sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+# Connect the socket to the port where the sever is listening
+server_address = ('localhost', 10000)
+print('connecting to {} port {}'.format(*server_address))
+sock.connect(server_address)
+try:
+    # Send data
+    message = b'This is the message.  It will be repeated.'
+    print('sending {!r}'.format(message))
+    sock.sendall(message)
+    # Look for the response
+    amount_received =0
+    amount_expected = len(message)
+    while amount_received < amount_expected:
+        data = sock.recv(256)
+        amount_received += len(data)
+        print('received {!r}'.format(data))
+finally:
+    print('closing socket')
+    sock.close()
